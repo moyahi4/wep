c4a861cd4818f1dd971f3bef6b2f154d55c11775
moyahi4 <131554264+moyahi4@users.noreply.github.com>
2024-09-07 21:14:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,30 +9,21 @@ yesBtn.addEventListener("click", () => {
   gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
 });
 
+// Function to check if two buttons overlap
+function isOverlapping(btn1, btn2) {
+  const rect1 = btn1.getBoundingClientRect();
+  const rect2 = btn2.getBoundingClientRect();
+
+  return !(
+    rect1.right < rect2.left ||
+    rect1.left > rect2.right ||
+    rect1.bottom < rect2.top ||
+    rect1.top > rect2.bottom
+  );
+}
+
 // Make the No button move randomly on hover
 noBtn.addEventListener("mouseover", () => {
   const wrapper = document.querySelector(".wrapper");
   const wrapperRect = wrapper.getBoundingClientRect();
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const yesBtnRect = yesBtn.getBoundingClientRect();
-
-  // Calculate max positions to ensure the button stays within the wrapper
-  const maxX = wrapperRect.width - noBtnRect.width;
-  const maxY = wrapperRect.height - noBtnRect.height;
-
-  let randomX, randomY;
-
-  do {
-    randomX = Math.floor(Math.random() * maxX);
-    randomY = Math.floor(Math.random() * maxY);
-  } while (
-    // Ensure the "No" button does not overlap the "Yes" button
-    randomX < yesBtnRect.right - wrapperRect.left &&
-    randomX + noBtnRect.width > yesBtnRect.left - wrapperRect.left &&
-    randomY < yesBtnRect.bottom - wrapperRect.top &&
-    randomY + noBtnRect.height > yesBtnRect.top - wrapperRect.top
-  );
-
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
-});
+  const no
